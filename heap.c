2d244371b8afff40c232d4ade8c0f09878a80c3f
Replit user <>
2023-05-11 14:18:53 +0000

    new try

--- heap.c
+++ heap.c
@@ -34,7 +34,7 @@ void heap_pop(Heap* pq){
 
 Heap* createHeap(){
   Heap* heap;
-  //heap = malloc(sizeof(heap));
+  heap = malloc(sizeof(Heap*));
  // heap->heapArray = (heapElem*) malloc (3 * sizeof(heapElem));
   heap->size = 0;
   heap->capac = 0;
