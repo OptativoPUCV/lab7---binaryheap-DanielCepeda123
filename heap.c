627853e353851aaba591818a26ecaff20f4f1a4b
Replit user <>
2023-05-11 17:22:03 +0000

    new try

--- heap.c
+++ heap.c
@@ -63,14 +63,14 @@ void heap_pop(Heap* pq){
  heapElem aux = pq->heapArray[0];
   pq->heapArray[0] = pq->heapArray[pq->size - 1];
   pq->heapArray[pq->size - 1] = aux;
-  //int posicion = 0;
+  int posicion = 0;
   
   while(1){
-    if(pq->heapArray[0].priority > pq->heapArray[2*0 + 1].priority) break;
+    if(pq->heapArray[posicion].priority > pq->heapArray[2*posicion + 1].priority) break;
     aux = pq->heapArray[0];
-    pq->heapArray[0] = pq->heapArray[2 *0 + 1];
-    pq->heapArray[2 * 0 + 1] = aux;
-    //posicion = posicion * 2 + 1;
+    pq->heapArray[posicion] = pq->heapArray[2 * posicion + 1];
+    pq->heapArray[2 * posicion + 1] = aux;
+    posicion = posicion * 2 + 1;
   }
   pq->size--;
   
