d399ca833a89ac7e44f8a2f67f6f3b286d7b9560
Replit user <>
2023-05-13 04:56:18 +0000

    new try

--- heap.c
+++ heap.c
@@ -91,13 +91,13 @@ void heap_pop(Heap* pq){
       }    
     }
 
-    /*if(pq->size == 2){
+    if(pq->size == 2){
       if (pq->heapArray[posicion].priority < pq->heapArray[2 * posicion + 1].priority){
         aux = pq->heapArray[posicion];
         pq->heapArray[posicion] = pq->heapArray[2 * posicion + 1];
         pq->heapArray[2 * posicion + 1] = aux;
       }
-    }*/
+    }
 
   break;
   }
