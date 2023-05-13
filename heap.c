62738bdad67e1d5117b55212e79d1b1215766799
Replit user <>
2023-05-13 03:14:25 +0000

    new try

--- heap.c
+++ heap.c
@@ -68,8 +68,8 @@ void heap_pop(Heap* pq){
   pq->size--;
   
   while(1){
-    if(pq->heapArray[posicion].priority >= pq->heapArray[2 * posicion + 1].priority) break;
-    if(pq->heapArray[posicion].priority >= pq->heapArray[2 * posicion + 2].priority) break;
+    if(pq->heapArray[posicion].priority >= pq->heapArray[2 * posicion + 1].priority && pq->heapArray[posicion].priority < pq->heapArray[2 * posicion + 2].priority) break;
+    if(pq->heapArray[posicion].priority >= pq->heapArray[2 * posicion + 2].priority && pq->heapArray[posicion].priority < pq->heapArray[2 * posicion + 1].priority) break;
 
     if(pq->heapArray[2 * posicion + 1].priority > pq->heapArray[2 * posicion + 2].priority){
       aux = pq->heapArray[posicion];
